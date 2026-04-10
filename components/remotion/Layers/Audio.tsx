9a2eb555c1c414d3a87f772d23c2f88f3460cff8
Majestic12-lab <seeyarjamali06@gmail.com>
2026-04-10 20:23:06 +0100

    fix: add audio buffer delay

--- components/remotion/Layers/Audio.tsx
+++ components/remotion/Layers/Audio.tsx
@@ -1,11 +1,1 @@
-import { Audio } from 'remotion'
-
-interface AudioLayerProps {
-  audioUrl: string | null
-}
-
-export function AudioLayer({ audioUrl }: AudioLayerProps) {
-  if (!audioUrl) return null
-  return <Audio src={audioUrl} />
-}
 
